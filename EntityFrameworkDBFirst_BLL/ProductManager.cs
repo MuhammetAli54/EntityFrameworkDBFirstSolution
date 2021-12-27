0f3bcfa8ad10ea55f25d842ef4ec02b564d72e98
Muhammet Ali <94957965+MuhammetAli54@users.noreply.github.com>
2021-12-27 09:31:43 +0300

    Add project files.

---
+++ EntityFrameworkDBFirst_BLL/ProductManager.cs
@@ -0,0 +1,49 @@
+﻿using EntityFrameworkDBFirst_BLL.ViewModels;
+using EntityFrameworkDBFirst_DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkDBFirst_BLL
+{
+    public class ProductManager
+    {
+        //Global Alan 
+        NORTHWNDEntities myDBContext = new NORTHWNDEntities();
+        public List<ProductViewModel> TumUrunleriGetir()
+        {
+            List<ProductViewModel> urunler = new List<ProductViewModel>();
+            var data = myDBContext.Products.ToList();
+
+            foreach (var item in data)
+            {
+                ProductViewModel p = new ProductViewModel()
+                {
+                    CategoryID = item.CategoryID,
+                    ProductID = item.ProductID,
+                    UnitPrice = item.UnitPrice,
+                    UnitsInStock = item.UnitsInStock,
+                    Discontinued = item.Discontinued,
+                    UnitsOnOrder = item.UnitsOnOrder,
+                    ReorderLevel=item.ReorderLevel,
+                    SupplierID=item.SupplierID,
+                    ProductName=item.ProductName,
+                    QuantityPerUnit=item.QuantityPerUnit
+
+                };
+
+                //CategoryName
+                //1. Yol
+                // p.CategoryName = item.Categories.CategoryName;
+
+                //2.Yol
+                p.CategoryName = myDBContext.Categories.FirstOrDefault(x => x.CategoryID == item.CategoryID)?.CategoryName;
+                urunler.Add(p);
+            }
+
+            return urunler;
+        }
+    }
+}
---
+++ EntityFrameworkDBFirst_WinFormUI/Form1.cs
@@ -0,0 +1,38 @@
+﻿using EntityFrameworkDBFirst_BLL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EntityFrameworkDBFirst_WinFormUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Global alan
+        ProductManager myProductManager = new ProductManager();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //form yüklenirken grid'e product bilgileri gelsin.
+            GrideProductlariGetir();
+        }
+
+        private void GrideProductlariGetir()
+        {
+            //BLL aracılığıyla listemiz gelecek
+            dataGridView1.DataSource = myProductManager.TumUrunleriGetir();
+            dataGridView1.Columns["SupplierID"].Visible = false;
+            dataGridView1.Columns["CategoryID"].Visible = false;
+
+        }
+    }
+}
